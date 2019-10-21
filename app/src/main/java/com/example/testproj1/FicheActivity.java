4216e0fcd66f37e7637f0d883c8e9d96bf697b9c
HyydroGene <ruddyfantelli@gmail.com>
2019-10-21 16:07:00 +0400

    Ajout Info XML FicheActivity

--- app/src/main/java/com/example/testproj1/FicheActivity.java
+++ app/src/main/java/com/example/testproj1/FicheActivity.java
@@ -4,18 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
+import com.example.testproj1.Personnages.Personnage;
+
+import org.w3c.dom.Text;
+
 public class FicheActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
         GestureDetector.OnDoubleTapListener {
 
-    TextView nameText;
+    // Texts de base (Informations --> Ne changera pas)
     TextView pvText, manaText;
     TextView atkpText, atkmText;
     TextView defpText, defmText;
+
+    // Texts qui contient les valeurs des statistiques --> Changera en fonction de l'état de la partie + personnage concerné
+    TextView nameText;
+    TextView pvValueText, manaValueText;
+    TextView atkpValueText, atkmValueText;
+    TextView defpValueText, defmValueText;
+
+    Personnage InfoPersonnage;
+
+
     private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
 
@@ -24,8 +39,46 @@ public class FicheActivity extends AppCompatActivity implements GestureDetector.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiche);
 
+        // On récupère la classe:
+        InfoPersonnage = (Personnage)getIntent().getSerializableExtra("infos");
+
+        // On charge les texts view:
+        loadTexts();
+
+        // On met les valeurs temps réel de la partie:
+        setValueTexts(InfoPersonnage);
+
         mDetector = new GestureDetectorCompat(this,this);
         mDetector.setOnDoubleTapListener(this);
+    }
+
+
+    public void loadTexts() {
+        // On met en place les Texts View:
+        nameText = (TextView)findViewById(R.id.nameText);
+        pvText = (TextView)findViewById(R.id.PVText);
+        manaText = (TextView)findViewById(R.id.manaText);
+        atkpText = (TextView)findViewById(R.id.ATKPText);
+        atkmText = (TextView)findViewById(R.id.ATKMText);
+        defpText = (TextView)findViewById(R.id.DEFPText);
+        defmText = (TextView)findViewById(R.id.DEFMText);
+
+        pvValueText = (TextView)findViewById(R.id.PVValue);
+        manaValueText = (TextView)findViewById(R.id.ManaValue);
+        atkpValueText = (TextView)findViewById(R.id.ATKPValue);
+        atkmValueText = (TextView)findViewById(R.id.ATKMValue);
+        defpValueText = (TextView)findViewById(R.id.DEFPValue);
+        defmValueText = (TextView)findViewById(R.id.DEFMValue);
+    }
+
+    public void setValueTexts(Personnage personnage) {
+        nameText.setText(personnage.getName());
+        pvValueText.setText(""+personnage.getPV());
+        manaValueText.setText(""+personnage.getMana());
+        atkpValueText.setText(""+personnage.getATKP().toString());
+        atkmValueText.setText(""+personnage.getATKM().toString());
+        defpValueText.setText(""+personnage.getDEFP());
+        defmValueText.setText(""+personnage.getDEFM());
     }
 
     public boolean onTouchEvent(MotionEvent event) {
