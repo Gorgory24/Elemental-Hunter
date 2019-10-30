9cc8494476ad1cd5be66719e49e51641040a40f5
Gorgory24 <hubert.gregory@live.fr>
2019-10-30 16:42:48 +0400

    Ajout du son d'ambiance

--- app/src/main/java/com/example/testproj1/MainActivity.java
+++ app/src/main/java/com/example/testproj1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     View MainVue;
+    private MediaPlayer mPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +27,34 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void play(View v) {
+        stop();
+        mPlayer = MediaPlayer.create(this, R.raw.connecte);
+        mPlayer.setOnCompletionListener(
+                new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        stop();
+                    }
+                }
+        );
+        mPlayer.start();
+    }
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
     public void Quit(View V){
         finish();
     }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stop();
+    }
 }
+
+// Sound made by Starrysky - Connecte toi
